08ea7b1b70fa63788b85e063bdc87101698cd31e
GUZYMAN <ger-guz44@hotmail.com>
2017-09-30 16:22:11 -0600

    Add files via upload

---
+++ rmlib/cmake-build-debug/CMakeFiles/rmlib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rmlib.dir/library.cpp.o"
+  "librmlib.pdb"
+  "librmlib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rmlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rmlib/library.h
@@ -0,0 +1,6 @@
+#ifndef RMLIB_LIBRARY_H
+#define RMLIB_LIBRARY_H
+
+void hello();
+
+#endif
