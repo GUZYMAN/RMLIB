24ea089e6637e4d9a375717fd55e7b46a226f7f7
GUZYMAN <ger-guz44@hotmail.com>
2017-10-02 01:19:30 -0600

    Add files via upload

---
+++ rmlib/PServer/cmake-build-debug/CMakeFiles/PServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PServer.dir/main.cpp.o"
+  "CMakeFiles/PServer.dir/library.cpp.o"
+  "PServer.pdb"
+  "PServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
